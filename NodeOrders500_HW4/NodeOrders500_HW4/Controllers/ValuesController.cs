f3b89da79f32f3b44e5c426066e20eb64215a4c5
Terry Consulting <truptidalia@gmail.com>
2021-05-05 10:19:23 -0700

    Creates initial Index UI page

--- NodeOrders500_HW4/NodeOrders500_HW4/Controllers/ValuesController.cs
+++ NodeOrders500_HW4/NodeOrders500_HW4/Controllers/ValuesController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NodeOrders500_HW4.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -9,9 +10,18 @@ namespace NodeOrders500_HW4.Controllers
 {
     public class ValuesController : ApiController
     {
+        NodeOrders500Entities entities = new NodeOrders500Entities();
+
         // GET api/values
         public IEnumerable<string> Get()
         {
+            List<CDTable> cds = entities.CDTables.Where(c => c.ListPrice > 13).ToList<CDTable>();
+
+            var list = from c in entities.CDTables
+            select new CityCDCount(c.CDname, c.YearReleased, 12);
+
+            List<CityCDCount> list1 = list.ToList();
+
             return new string[] { "value1", "value2" };
         }
 
